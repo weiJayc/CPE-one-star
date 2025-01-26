45ce5aac1bfe028eacc36267febf28179f5fb75c
weru6129PC <weru6129@gmail.com>
2025-01-27 00:49:39 +0800

    simplified 10222 code

--- 10222 - Decode the Mad man.cpp
+++ 10222 - Decode the Mad man.cpp
@@ -4,29 +4,25 @@
 using namespace std;
 
 int main() {
-    vector< vector<char> > vec = {
-        {'`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '='},
-        {'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\'},
-        {'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\''},
-        {'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/'}
+    vector<char> vec = {
+        '`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=',
+        'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\',
+        'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'',
+        'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/'
     };
     
     string str;
     while(getline(cin, str)) {
         string output;
-        for(char& c : str) {
-            c = tolower(c);
-        }
         for(char c : str) {
-            if(c == ' ') {
-                output += " ";
-                continue;
+            c = tolower(c);
+
+            auto it = find(vec.begin(), vec.end(), c);
+            if(it != vec.end()) {
+                output += *(it - 2);
             }
-            for(auto& j : vec) {
-                auto it = find(j.begin(), j.end(), c);
-                if(it != j.end()) {
-                    output += *(it - 2);
-                }
+            else {
+                output += " ";
             }
         }
         cout << output << endl;
